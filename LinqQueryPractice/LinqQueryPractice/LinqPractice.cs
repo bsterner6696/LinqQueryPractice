1f8a5b046816f5683577344e981257457ba7da3f
Benjamin <bsterner6696@gmail.com>
2016-10-14 08:34:51 -0500

    First linq practice problem finished

---
+++ LinqQueryPractice/LinqQueryPractice/LinqPractice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqQueryPractice
+{
+    class LinqPractice
+    {
+
+        public List<string> RemoveRepeatedValues(List<string> values)
+        {
+            var uniqueValues = values.Distinct();
+            List<string> results = new List<string>();
+            foreach (string x in uniqueValues)
+            {
+                results.Add(x);
+            }
+            return results;
+        } 
+    }
+}
---
+++ LinqQueryPractice/LinqQueryPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqQueryPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinqPractice linq = new LinqPractice();
+            List<string> examples = new List<string> { "x", "x", "gon", "x", "give", "gon", "it", "to", "to", "x", "ya" };
+            List<string> results = linq.RemoveRepeatedValues(examples);
+            foreach (string line in results)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
