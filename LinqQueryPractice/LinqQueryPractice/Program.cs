a3d43bb5b8141589ee076693498be33b09534088
Benjamin <bsterner6696@gmail.com>
2016-10-14 10:35:51 -0500

    organized string of grades

--- LinqQueryPractice/LinqQueryPractice/Program.cs
+++ LinqQueryPractice/LinqQueryPractice/Program.cs
@@ -14,7 +14,7 @@ namespace LinqQueryPractice
             LinqPractice linq = new LinqPractice();
             List<string> examples = new List<string> { "x", "x", "gon", "x", "give", "gon", "it", "to", "to", "x", "ya" };
             string gradeLine = "100,105,105,110";
-            List<string> grades = new List<string> { "100,105,105,110", "95, 100, 50, 90", "100, 90, 85, 95", "100, 100, 100, 100" };
+            List<string> grades = new List<string> { "Student A: 100, 105, 105, 110", "Student B: 95, 100, 50, 90", "Student C: 100, 90, 85, 95", "Student D: 100, 100, 100, 100" };
             List<string> results = linq.RemoveRepeatedValues(examples);
             foreach (string line in results)
             {
