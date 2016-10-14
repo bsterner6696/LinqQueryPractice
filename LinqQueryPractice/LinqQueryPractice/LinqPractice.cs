5c6675674fd7296bd53063105a50146889f717e8
Benjamin <bsterner6696@gmail.com>
2016-10-14 10:34:04 -0500

    Finished second problem

--- LinqQueryPractice/LinqQueryPractice/LinqPractice.cs
+++ LinqQueryPractice/LinqQueryPractice/LinqPractice.cs
@@ -19,5 +19,61 @@ namespace LinqQueryPractice
             }
             return results;
         } 
+
+        public List<decimal> ConvertToDecimalList(string line)
+        {
+            char[] characters = line.ToCharArray();
+            List<decimal> results = new List<decimal>();
+            int numberPlace = 1;
+            decimal storedNumber = 0;
+            bool wasNumber = false;
+            int parsedNumber;
+            for (int x = 0; x < characters.Count(); x++)
+            {
+                if (Int32.TryParse(characters[characters.Count() - 1 - x].ToString(), out parsedNumber))
+                {
+                    storedNumber += (parsedNumber * numberPlace);
+                    numberPlace *= 10;
+                    wasNumber = true;
+                }
+                else if (!Int32.TryParse(characters[characters.Count() - 1 - x].ToString(), out parsedNumber) && wasNumber == true)
+                {
+                    results.Add(storedNumber);
+                    storedNumber = 0;
+                    numberPlace = 1;
+                    wasNumber = false;
+                }
+                if (characters.Count() - 1 -x == 0 && wasNumber)
+                {
+                    results.Add(storedNumber);
+                }
+            }
+            return results;
+        }
+
+        public List<decimal> RemoveMinValue(List<decimal> inputs)
+        {
+            decimal lowest = inputs.Min();
+            inputs.Remove(lowest);
+            return inputs;
+        }
+
+        public decimal AverageDecimals(List<decimal> inputs)
+        {
+            decimal output = inputs.Average();
+            return output;
+        }
+
+        public decimal AverageGradeStrings(List<string> inputs)
+        {
+            List<decimal> averageGrades = new List<decimal>();
+            foreach (string line in inputs)
+            {
+                decimal result = AverageDecimals(RemoveMinValue(ConvertToDecimalList(line)));
+                averageGrades.Add(result);
+            }
+            decimal output = averageGrades.Average();
+            return output;
+        }
     }
 }
--- LinqQueryPractice/LinqQueryPractice/Program.cs
+++ LinqQueryPractice/LinqQueryPractice/Program.cs
@@ -13,11 +13,22 @@ namespace LinqQueryPractice
 
             LinqPractice linq = new LinqPractice();
             List<string> examples = new List<string> { "x", "x", "gon", "x", "give", "gon", "it", "to", "to", "x", "ya" };
+            string gradeLine = "100,105,105,110";
+            List<string> grades = new List<string> { "100,105,105,110", "95, 100, 50, 90", "100, 90, 85, 95", "100, 100, 100, 100" };
             List<string> results = linq.RemoveRepeatedValues(examples);
             foreach (string line in results)
             {
                 Console.WriteLine(line);
             }
+          
+            List<decimal> intResults = linq.ConvertToDecimalList(gradeLine);
+            intResults = linq.RemoveMinValue(intResults);
+
+            foreach (decimal x in intResults)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine(linq.AverageGradeStrings(grades));
             Console.ReadLine();
         }
     }
